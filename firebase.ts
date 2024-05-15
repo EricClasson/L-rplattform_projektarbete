a10ecff0da5a98e6752d19cbcf4e05744007ab1e
Roudi22 <roudi.alzkemi@gmail.com>
2024-05-15 21:30:31 +0200

    implementera "Authentication" från firebase

--- firebase.ts
+++ firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -13,4 +14,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 export const p_formCollection = collection(db, "test");
+// users collection
+export const usersCollection = collection(db, "users");
