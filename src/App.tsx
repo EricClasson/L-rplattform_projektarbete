807f05a03f95d17c5fd2d422ac899b9e312600f9
Roudi22 <roudi.alzkemi@gmail.com>
2024-05-15 21:30:54 +0200

    Lägga till andra routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Test from "./componets/Test";
+import Login from "./componets/Login";
+import Dashboard from "./componets/Dashboard";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/hello" element={<Test />} />
+        <Route path="/" element={<Login/>} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
     </>
   );
