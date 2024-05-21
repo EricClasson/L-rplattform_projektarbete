7850db5cc080ee15fa63e8288d6ea83f3b678ada
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-05-21 14:02:01 +0200

    Adding edit function and style

--- src/componets/Dashboard.tsx
+++ src/componets/Dashboard.tsx
@@ -12,7 +12,7 @@ const Dashboard = () => {
       <div className="grid grid-cols-1 gap-4 breakPoint:grid-cols-[170px_1fr] lg:gap-8 ">
         <Sidebar />
         <div>
-          <h2 className="text-center">Welcome to the dashboard {role}</h2>
+          <h2 className="text-center py-5">Welcome to the dashboard {role}</h2>
           {role === "teacher" && <Publish />}
           <Outlet />
         </div>
