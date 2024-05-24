37ab6eb66c92e0a3afa6ac9ddc9547abf8612018
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-05-24 11:15:14 +0200

    uppdating interface student

--- src/componets/StudentList/StudentList.tsx
+++ src/componets/StudentList/StudentList.tsx
@@ -1,23 +1,17 @@
-
 import React, { useState, useEffect } from "react";
-import {
-  onSnapshot,
-  doc,
-  collection,
-  deleteDoc,
-} from "firebase/firestore";
+import { onSnapshot, doc, collection, deleteDoc } from "firebase/firestore";
 import { db } from "../../../firebase";
 import { Link } from "react-router-dom";
 
-
 interface Student {
-    id: string;
-    role: string;
-    email: string;
+  id: string;
+  role: string;
+  email: string;
+  firstname: string;
+  lastname: string;
 }
 
 const StudentList = () => {
-
   const [studentList, setStudentList] = useState<Student[]>([]);
   const user = window.localStorage.getItem("user") || "";
   const role = JSON.parse(user).role;
@@ -38,68 +32,67 @@ const StudentList = () => {
     await deleteDoc(doc(db, "users", id));
   };
 
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "users"), (snapshot) => {
+      const list: Student[] = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Student[];
 
-    useEffect(() => {
-        const unsub = onSnapshot(collection(db, 'users'), (snapshot) => {
-            const list: Student[] = snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            })) as Student[];
+      const result = list.filter((student) => student.role === "student");
+      setStudentList(result);
+    });
+    return () => unsub();
+  }, []);
 
-            const result = list.filter((student) => student.role === 'student');
-            setStudentList(result);
-        });
-        return () => unsub();
-    }, []);
+  return (
+    <div className="grid justify-items-center grid-flow-row ">
+      <h2 className="font-semibold py-6">StudentList</h2>
+      <div className="flex flex-row justify-between w-full px-5">
+        <div></div>
+        <div></div>
+        {role === "teacher" && (
+          <div className="">
+            <button className="button">
+              <Link to={"/register"}>New Student</Link>
+            </button>
+          </div>
+        )}
+      </div>
 
-    return (
-        <div className="grid justify-items-center grid-flow-row ">
-            <h2 className="font-semibold py-6">StudentList</h2>
-            <div className="flex flex-row justify-between w-full px-5">
-                <div></div>
-                <div></div>
-                {role === 'teacher' && (
-                    <div className="">
-                        <button className="button">
-                            <Link to={'/register'}>New Student</Link>
-                        </button>
-                    </div>
+      <ul className="grid grid-cols-2 py-10 gap-10 max-breakPoint3:flex max-breakPoint3:flex-col  breakPoint2:grid-cols-3 ">
+        {studentList.map((student) => (
+          <li key={student.email} className="hover:shadow-lg">
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure className="px-8 pt-8 flex flex-col flex-wrap items-center">
+                <div className="btn btn-circle">
+                  <p>{student.firstname.slice(0, 1).toUpperCase()}</p>
+                </div>
+                <div className="flex flex-row gap-1">
+                  <p>{student.firstname}</p>
+                  <p>{student.lastname}</p>
+                </div>
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">{student.email}</h2>
+                <p>{student.role}</p>
+                {role === "teacher" && (
+                  <div className="card-actions">
+                    <button
+                      className="button"
+                      onClick={() => handleDelete(student.id)}
+                    >
+                      Remove
+                    </button>
+                  </div>
                 )}
+              </div>
             </div>
-
-            <ul className="grid grid-cols-2 py-10 gap-10 max-breakPoint3:flex max-breakPoint3:flex-col  breakPoint2:grid-cols-3 ">
-                {studentList.map((student) => (
-                    <li key={student.email} className="hover:shadow-lg">
-                        <div className="card w-96 bg-base-100 shadow-xl">
-                            <figure className="px-8 pt-8 flex flex-col flex-wrap items-center">
-                                <div className="btn btn-circle">
-                                    <p>{student.firstname.slice(0, 1).toUpperCase()}</p>
-                                </div>
-                                <div className="flex flex-row gap-1">
-                                    <p>{student.firstname}</p>
-                                    <p>{student.lastname}</p>
-                                </div>
-                            </figure>
-                            <div className="card-body items-center text-center">
-                                <h2 className="card-title">{student.email}</h2>
-                                <p>{student.role}</p>
-                                {role === 'teacher' && (
-                                    <div className="card-actions">
-                                        <button
-                                            className="button"
-                                            onClick={() => handleDelete(student.id)}
-                                        >
-                                            Remove
-                                        </button>
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default StudentList;
