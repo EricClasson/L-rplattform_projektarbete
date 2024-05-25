f1253889ff4e665ced48b0453bb771fa24c3f9cc
Van Loc Doan <vaan.l.doan@gmail.com>
2024-05-25 23:57:30 +0200

    update with sonner

--- src/componets/Logout.tsx
+++ src/componets/Logout.tsx
@@ -1,6 +1,7 @@
 import { signOut } from 'firebase/auth';
 import { auth } from '../../firebase';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 function Logout() {
     const navigate = useNavigate();
@@ -8,7 +9,26 @@ function Logout() {
     const userlogout = async () => {
         try {
             await signOut(auth).then(() => {
-                window.localStorage.removeItem('user');
+                toast('Successfully logged out!', {
+                    className: 'bg-green-100 flex items-center',
+                    duration: 5000,
+                    icon: (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                            />
+                        </svg>
+                    ),
+                });
                 navigate('/');
             });
         } catch (error) {
@@ -16,9 +36,11 @@ function Logout() {
         }
     };
     return (
-        <div className='flex flex-col items-center justify-center p-3 gap-2 shadow-2xl border'>
-            <h1 className=' text-xl font-semibold'>Sign Out Page</h1>
-            <button onClick={userlogout} className='button'>SignOut</button>
+        <div className="flex flex-col items-center justify-center p-3 gap-2 shadow-md border">
+            <h1 className=" text-xl font-semibold">Sign Out Page</h1>
+            <button onClick={userlogout} className="button">
+                SignOut
+            </button>
         </div>
     );
 }
