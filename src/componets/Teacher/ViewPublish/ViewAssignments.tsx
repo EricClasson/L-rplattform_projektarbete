321382a86edea57bb6aaf7d7ff8a33f33a7c3c0f
Van Loc Doan <vaan.l.doan@gmail.com>
2024-05-25 16:26:18 +0200

    update/fixed-viewassignments

--- src/componets/Teacher/ViewPublish/ViewAssignments.tsx
+++ src/componets/Teacher/ViewPublish/ViewAssignments.tsx
@@ -1,150 +1,146 @@
-import { useState, useEffect } from "react";
-import {
-  collection,
-  onSnapshot,
-  doc,
-  deleteDoc,
-  updateDoc,
-} from "firebase/firestore";
-import { db } from "../../../../firebase";
-import { PublishDoc } from "../Publish/Publish";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { collection, onSnapshot, doc, deleteDoc, updateDoc } from 'firebase/firestore';
+import { db } from '../../../../firebase';
+import { PublishDoc } from '../Publish/Publish';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../../hooks/useAuth';
 export default function ViewAssignments() {
-  const [Assignments, setAssignment] = useState<PublishDoc[]>([]);
-  const [editId, setEditId] = useState<string | null>(null);
-  const [titleChange, setTitleChange] = useState<string>("");
-  const [informationChange, setInformationChange] = useState<string>("");
-  const user = window.localStorage.getItem("user") || "";
-  const role = JSON.parse(user).role;
-  useEffect(() => {
-    const unsub = onSnapshot(collection(db, "Assignments"), (snapshot) => {
-      const Publish: PublishDoc[] = snapshot.docs.map((doc) => ({
-        ...(doc.data() as PublishDoc),
-        id: doc.id,
-      }));
-      setAssignment(Publish);
-      console.log(Publish);
-    });
-    return () => unsub();
-  }, []);
-  // Get the assignment details by id
-  
+    const [Assignments, setAssignment] = useState<PublishDoc[]>([]);
+    const [editId, setEditId] = useState<string | null>(null);
+    const [titleChange, setTitleChange] = useState<string>('');
+    const [informationChange, setInformationChange] = useState<string>('');
 
-  
-  const handleDelete = async (id: string) => {
-    await deleteDoc(doc(db, "Assignments", id));
-  };
+    // const user = window.localStorage.getItem("user") || "";
+    // const role = JSON.parse(user).role;
 
-  const handleUpdate = async (id: string) => {
-    try {
-      await updateDoc(doc(db, "Assignments", id), {
-        title: titleChange,
-        information: informationChange,
-      });
-      setEditId(null);
-      setTitleChange("");
-      setInformationChange("");
-    } catch (error) {
-      console.log("It didn't update", error);
-    }
-  };
+    const { user, userData, loading } = useAuth();
+    const role = userData?.role;
 
-  return (
-    <div className="flex flex-col">
-      <h2 className="text-center font-semibold py-6">Assignments</h2>
-      <ul className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
-        {Assignments.map((index) => (
-          <li
-            key={index.id}
-            className="rounded-lg border border-gray-100 bg-white p-4 shadow-sm transition hover:shadow-lg sm:p-6 flex flex-col gap-4"
-          >
-            {editId === index.id ? (
-              <>
-                <label>Title</label>
-                <input
-                  placeholder="Title"
-                  className="input border border-black "
-                  type="text"
-                  value={titleChange}
-                  onChange={(e) => setTitleChange(e.target.value)}
-                />
-                <label>Description</label>
-                <textarea
-                  className="input border border-black"
-                  placeholder="Information"
-                  rows="8"
-                  value={informationChange}
-                  onChange={(e) => setInformationChange(e.target.value)}
-                ></textarea>
-                <button
-                  className="buttonGreen"
-                  onClick={() => handleUpdate(index.id)}
-                >
-                  Update
-                </button>
-              </>
-            ) : (
-              <>
-                <div className="flex flex-col gap-4">
-                  <div>
-                    <div className="flex flex-row gap-1">
-                      <p className="text-xs"> Publish date: </p>
-                      <p className="text-xs font-semibold ">
-                        {index.date.slice(0, 10)}
-                      </p>
-                    </div>
-                    <div className="flex flex-row gap-1">
-                      <p className="text-xs"> Deadline: </p>
-                      <p className="text-xs font-semibold ">
-                        {index.dueDate ? index.dueDate.slice(0, 10) : "No deadline"}
-                      </p>
-                    </div>
-                  </div>
+    useEffect(() => {
+        const unsub = onSnapshot(collection(db, 'Assignments'), (snapshot) => {
+            const Publish: PublishDoc[] = snapshot.docs.map((doc) => ({
+                ...(doc.data() as PublishDoc),
+                id: doc.id,
+            }));
+            setAssignment(Publish);
+            console.log(Publish);
+        });
+        return () => unsub();
+    }, []);
+    // Get the assignment details by id
 
-                  <div>
-                    <h2>Title: {index.title}</h2>
-                    <h3>Type: {index.option}</h3>
-                  </div>
-                </div>
+    const handleDelete = async (id: string) => {
+        await deleteDoc(doc(db, 'Assignments', id));
+    };
 
-                <div className="flex flex-col">
-                  <h3>Description:</h3>{" "}
-                  <p className="text-xs">{index.information}</p>
-                </div>
-              </>
-            )}
-            <div className="flex flex-row items-center gap-5">
-              {
-                role === "teacher" && (
-              <button
-                onClick={() => handleDelete(index.id)}
-                className="button py-5"
-              >
-                Delete
-              </button>
-                  
-                )
-              }
-              {editId !== index.id && role === "teacher" && (
-                <button
-                  className="buttonTeal w-16"
-                  onClick={() => {
-                    setEditId(index.id);
-                    setTitleChange(index.title);
-                    setInformationChange(index.information);
-                  }}
-                >
-                  Edit
-                </button>
-              )}
-              <Link to={`/dashboard/GetAssignmentsTeacher/${index.id}`}>
-              <button className="buttonRed w-16">View</button>
-              
-              </Link>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    const handleUpdate = async (id: string) => {
+        try {
+            await updateDoc(doc(db, 'Assignments', id), {
+                title: titleChange,
+                information: informationChange,
+            });
+            setEditId(null);
+            setTitleChange('');
+            setInformationChange('');
+        } catch (error) {
+            console.log("It didn't update", error);
+        }
+    };
+
+    return (
+        <div className="flex flex-col">
+            <h2 className="text-center font-semibold py-6">Assignments</h2>
+            <ul className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
+                {Assignments.map((index) => (
+                    <li
+                        key={index.id}
+                        className="rounded-lg border border-gray-100 bg-white p-4 shadow-sm transition hover:shadow-lg sm:p-6 flex flex-col gap-4"
+                    >
+                        {editId === index.id ? (
+                            <>
+                                <label>Title</label>
+                                <input
+                                    placeholder="Title"
+                                    className="input border border-black "
+                                    type="text"
+                                    value={titleChange}
+                                    onChange={(e) => setTitleChange(e.target.value)}
+                                />
+                                <label>Description</label>
+                                <textarea
+                                    className="input border border-black"
+                                    placeholder="Information"
+                                    rows="8"
+                                    value={informationChange}
+                                    onChange={(e) => setInformationChange(e.target.value)}
+                                ></textarea>
+                                <button
+                                    className="buttonGreen"
+                                    onClick={() => handleUpdate(index.id)}
+                                >
+                                    Update
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <div className="flex flex-col gap-4">
+                                    <div>
+                                        <div className="flex flex-row gap-1">
+                                            <p className="text-xs"> Publish date: </p>
+                                            <p className="text-xs font-semibold ">
+                                                {index.date.slice(0, 10)}
+                                            </p>
+                                        </div>
+                                        <div className="flex flex-row gap-1">
+                                            <p className="text-xs"> Deadline: </p>
+                                            <p className="text-xs font-semibold ">
+                                                {index.dueDate
+                                                    ? index.dueDate.slice(0, 10)
+                                                    : 'No deadline'}
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <h2>Title: {index.title}</h2>
+                                        <h3>Type: {index.option}</h3>
+                                    </div>
+                                </div>
+
+                                <div className="flex flex-col">
+                                    <h3>Description:</h3>{' '}
+                                    <p className="text-xs">{index.information}</p>
+                                </div>
+                            </>
+                        )}
+                        <div className="flex flex-row items-center gap-5">
+                            {role === 'teacher' && (
+                                <button
+                                    onClick={() => handleDelete(index.id)}
+                                    className="button py-5"
+                                >
+                                    Delete
+                                </button>
+                            )}
+                            {editId !== index.id && role === 'teacher' && (
+                                <button
+                                    className="buttonTeal w-16"
+                                    onClick={() => {
+                                        setEditId(index.id);
+                                        setTitleChange(index.title);
+                                        setInformationChange(index.information);
+                                    }}
+                                >
+                                    Edit
+                                </button>
+                            )}
+                            <Link to={`/dashboard/GetAssignmentsTeacher/${index.id}`}>
+                                <button className="button">View</button>
+                            </Link>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
